1bdbea30ec27d028b7e4268adf3b2dfaddba584d
manojTheDev1232 <manojsmunda.dev@gmail.com>
2024-11-06 16:45:09 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict" //treat all Js code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+
+let name ="manoj"
+let age = 18
+let isLoggedIn = true
+let state = null
+
+//number => 2 to power 53
+//bigInt
+//string => ""
+//boolean => true/false
+//null =>standalone value
+//undefined =>
+//symbol => unique
+
+//object
+
+console.log(typeof null) // Null is a object not a type
+console.log(typeof undefined) // undefined
