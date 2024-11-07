86f6324aee080c29f720bd8bd2c5450faa1da253
manojTheDev1232 <manojsmunda.dev@gmail.com>
2024-11-07 02:41:34 +0000

    conversionOfDataTypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -16,4 +16,20 @@ let valueInNumber = Number(score)
 // "33abc" => NaN
 // true =>1 : false => 0
 
-let isLoggedIn = 1
+
+let isLoggedIn = "manoj"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 =>true; 0 => false
+// "" => false
+// "manoj" => true
+
+/* String conversion */
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
