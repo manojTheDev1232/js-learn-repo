1c771805f8f61844e36a63a98c1383f9dab4efd9
manojTheDev1232 <manojsmunda.dev@gmail.com>
2024-11-07 15:26:00 +0000

    string

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,27 @@
+const name = "manoj"
+const repoCount = 50
+
+// console.log(name + repoCount+" Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); //String interpolation better than above one
+
+const gameName = new String('hitesh')
+
+// console.log(gameName[0])
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+
+
+console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString)
+
+
+
--- datatypes-summary.js
+++ datatypes-summary.js
@@ -34,3 +34,36 @@ const myFunction = function(){
     console.log("Hello World")
 }
 console.log(typeof myFunction); // function
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//Stack (Primitive) , Heap(Non-Primitive)
+
+// from stack , we will get a copied value but from heap we will get a reference value(orignal can be changed)
+
+let myYoutubeName = "abc"
+
+let anotherName = myYoutubeName
+anotherName = "bbc"
+console.log(anotherName)
+console.log(myYoutubeName)
+
+
+let name = 'John'; // allocates memory for a string
+const age = 24; // allocates memory for a number
+
+name = 'John Doe'; // allocates memory for a new string
+const firstName = name.slice(0,6); // allocates memory for a new string
+console.log(name)
+
+
+let userOne = {
+    email: "manoj@gmail.com"
+}
+
+let userTwo = userOne
+userTwo.email = "manojsingh@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
